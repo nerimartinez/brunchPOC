9b311dd0d4423cd34240a2971cd15bece5ff39ac
Usuario <usuario@MacBook-Pro-de-Santiago.local>
2016-12-02 09:41:14 -0300

    first commit

---
+++ app/initialize.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // do your setup here
+  console.log('Initialized app');
+});
+
+import React from 'react';
+
+//var React = require('react');
+var ReactDOM = require('react-dom');
+  console.log('react');
+  console.log(ReactDOM);
+
+class HelloWorld extends React.Component {. render() {. return <p>Hello, world!</p>;. } } 
