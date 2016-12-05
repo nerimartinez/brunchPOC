efdcc2d36f586453dda25d930ed7b99e3af31190
Usuario <usuario@MacBook-Pro-de-Santiago.local>
2016-12-05 17:40:37 -0300

    style and refactor

--- app/containers/todos-list.js
+++ app/containers/todos-list.js
@@ -4,14 +4,14 @@ import {connect} from 'react-redux';
 import {delTodo, checkTodo} from '../actions/index';
 
 class TodoList extends Component{
-  
+
   createListItems(){
     return this.props.todos.map((todo) =>{
     return (
       <li key={todo.id}>
         <input type="checkbox" checked={todo.checked} onChange={() => this.props.checkTodo(todo)}/>
-        <span>{todo.title} </span>
-        <span onClick={() => this.props.delTodo(todo)}>X</span>
+        <span>{todo.title} - </span>
+        <span onClick={() => this.props.delTodo(todo)} style={{color: 'red'}}>X</span>
       </li>
     );
   });
--- app/initialize.js
+++ app/initialize.js
@@ -1,18 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
-  var ReactDOM = require('react-dom');
-  
-    const element = (
-      <h1>
-        Hello, d!
-      </h1>
-    );
 
-    ReactDOM.render(
-      element,
-      document.getElementById('app')
-    );
 });
 import React from 'react';
 import Redux from 'redux';
